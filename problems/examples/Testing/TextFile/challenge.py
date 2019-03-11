a072d13e0abf13dd4cc013c6337fe5ac4410ecbb
angie <angiexchen99@gmail.com>
2019-03-11 14:53:00 -0500

    revised textfile problem

--- problems/examples/Testing/TextFile/challenge.py
+++ problems/examples/Testing/TextFile/challenge.py
@@ -1,2 +1,2 @@
 from hacksport.problem_templates import CompiledBinary
-Problem = CompiledBinary(sources=["myproblem.cpp"], static_flag="MarWeiChen2020")
+Problem = CompiledBinary(sources=["vuln.c"], static_flag="MarWeiChen2020")
