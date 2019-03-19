c0aceb9de18cfde81623202ab881deb73a295015
angie <angiexchen99@gmail.com>
2019-03-19 15:01:03 -0500

    added ColorSquare

---
+++ problems/examples/cryptography/ColorSquare/vuln.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+void testPrint(){
+printf("Hello, World! \n");
+}
+
+int main(int argc, char **argv){
+  // Set the gid to the effective gid
+  // this prevents /bin/sh from dropping the privileges
+  gid_t gid = getegid();
+  setresgid(gid, gid, gid);
+
+  testPrint();
+  return 0;
+}
